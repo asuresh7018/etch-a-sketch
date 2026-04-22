4851531c90e62be7b9cdc388af88d6074b7969b1
Arjun Suresh <asuresh7018@gmail.com>
2026-04-22 09:12:09 +0000

    Spawn rows dynamically

--- code.js
+++ code.js
@@ -1,13 +1,42 @@
-const row = document.querySelector(".row");
+const row = document.createElement("div");
+row.className = "row";
+const tile = document.createElement("tile");
+tile.className = "tile";
 const canvas = document.querySelector("#canvas");
-const tile = document.querySelector(".tile");
 
-// Add tiles to row
-for (let i = 0; i < 15; i++) {
-    row.appendChild(tile.cloneNode(true));
+function clearGrid() {
+    canvas.childNodes.forEach((node) => {
+        canvas.removeChild(node);
+    });
 }
 
-// Add rows to canvas
-for (let i = 0; i < 15; i++) {
-    canvas.appendChild(row.cloneNode(true));
+function createGrid(width) {
+    // Create a single row
+        canvas.appendChild(row.cloneNode(true));
+        for (let i = 0; i < width-1; i++) {
+        row.appendChild(tile.cloneNode(true));
+        }
+
+    // Add tiles to row
+    for (let i = 0; i < width; i++) {
+        row.appendChild(tile.cloneNode(true));
+    }
+
+    // Add rows to canvas
+    for (let i = 0; i < width; i++) {
+        canvas.appendChild(row.cloneNode(true));
+    }
+}
+
+function spawnRow(width) {
+    const newRow = row.cloneNode(true)
+    canvas.appendChild(newRow);
+        for (let i = 0; i < width; i++) {
+        newRow.appendChild(tile.cloneNode(true));
+        }
+}
+
+clearGrid();
+for (let i = 0; i < 16; i++) {
+    spawnRow(16);
 }
